b2ef695df0e60b12921317771880c9d2d78dd59d
Milten <Milten198@gmail.com>
2016-04-23 11:04:03 +0200

    Ready example of handler

---
+++ app/src/main/java/com/example/android/handlersimpleexample/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.android.handlersimpleexample;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    Thread thread;
+    Handler handler;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        thread = new Thread(new MyThread());
+        thread.start();
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                progressBar.setProgress(msg.arg1);
+            }
+        };
+    }
+
+    class MyThread implements Runnable {
+
+        @Override
+        public void run() {
+
+            for (int i = 0; i < 100; i++) {
+                Message message = Message.obtain();
+                message.arg1 = i;
+                handler.sendMessage(message);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+
+                }
+            }
+        }
+    }
+}
